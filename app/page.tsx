1bf1a592586ff3df8249b9f1f6d8f215416e2aaa
leticiaOntiveros <167127935+leticiaOntiveros@users.noreply.github.com>
2025-05-29 16:03:22 -0600

    Delete app directory

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  redirect("/contenedoresInfo/datosBasicos"); // Redirige automáticamente a la página de contenedores
-  return null; // No necesitas renderizar nada aquí
-}
